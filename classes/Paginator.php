0c8619d543ff17a0940f56ca09bb83fafa9ecaac
Doychin <doychinborisov84@gmail.com>
2023-01-16 16:33:06 +0200

    (wip) Paginator class and refactoring

--- classes/Paginator.php
+++ classes/Paginator.php
@@ -5,78 +5,170 @@
  */
 class Paginator
 {
-    private $total_images = 50; // total item incoming API, Database, CONST int ...
+    public static $total_images = 50; // total item incoming API, Database, CONST int ...
 
     private $images_per_page = 12; // ?User Input -- our needs for the grid
-    // $images_per_row = 4; // ?? CONST for easy and more gridable rows our needs for the grid
 
-    // $total_pages = ceil($total_images/$images_per_page);
+    private $images_per_row = 4; // ?? CONST for easy and more gridable rows our needs for the grid
 
-    // $total_rows = ceil($images_per_page / $images_per_row);
-    // $total_rows = ceil($images_per_page / 4); // total rows is default=hardcoded 
-    
-//     $current_page = $_GET['page'] >= $total_pages ? $total_pages : ($_GET['page'] < 0 ? 1 : $_GET['page']); // first|last|$_GET[page] -> page
-//     $previous_page = $current_page - 1;
-//     $next_page = $current_page <= 0 ? 2 : $current_page + 1;
+    private $current_page;
 
-//     $counter_images_current_page = get_current_page_image_offset($current_page, $images_per_page);
-    
-//     // Get the current page offset of items
-//     function get_current_page_image_offset($current_page, $images_per_page) {
-//         $offset = (($current_page * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item       
+    public function __construct()
+    {
+        $this->current_page = $this->getCurrentPage();
+    }
 
-//         if($current_page <= 0){
-//             // $offset = ((1 * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item
-//             $offset = 1;
-//         }
+    /**
+     * Get previous page class
+     */    
+    public function getPreviousPageClass()
+    {
+        return $this->getCurrentPage() <= 1 ? 'disabled' : '';
+    }
 
-//         return $offset;
-//     };
+    /**
+     * Get next page class
+     */    
+    public function getNextPageClass()
+    {
+        return $this->getCurrentPage() >= $this->getTotalPages() ? "disabled" : "";
+    }
 
-// function nextPage($current_page, $total_pages, $last_page = false){
-//     $uri = "index.php?page=";
-//     if($current_page == $total_pages){
-//         return '#';        
-//     }elseif ($last_page == true){
-//         return $next_page = "{$uri}{$total_pages}";
-//     }
-//     else {
-//         $next_page = $current_page + 1;
-//         return "{$uri}{$next_page}";
-//     }
-// }
+    /**
+     *  Get last page class
+     */    
+    public function getLastPageClass()
+    {
+        return $this->getCurrentPage() >= $this->getTotalPages() ? "link-danger" : "";
+    }
 
-// function previousPage($current_page, $total_pages){
-//     if($current_page == 1){
-//         return '#';
-//     }else if($current_page >= $total_pages){
-//         return $total_pages - 1;
-//     }
-// }
-
-// // TODO: to be replaced by Pagination class methods
-// // Get previous page class
-// $previous_page_class = function() use ($current_page) {
-//     return $current_page <= 1 ? 'disabled' : '';
-// };
-
-// // Get next page class
-// $next_page_class = function () use ($current_page, $total_pages) {
-//     return $current_page >= $total_pages ? "disabled" : "";    
-// };
-
-// // Get last page class
-// $last_page_class = function () use ($current_page, $total_pages) {
-//     return $current_page >= $total_pages ? "link-danger" : "";
-// };
-
+    /**
+     * Get total pages based on the items loaded  
+     *      
+     */
     public function getTotalPages()
     {
-        return $total_pages = ceil($this->total_images / $this->images_per_page);
+        return $total_pages = ceil(self::$total_images / $this->images_per_page);
     }
+
+    /**
+     * 
+     * Get total rows per page
+     */
+    public function getTotalRows()
+    {
+        return $total_rows = ceil($this->images_per_page / $this->images_per_row);        
+    }
+
+    /**
+     * Get current | min/max page
+     * 
+     */
+    public function getCurrentPage() 
+    {
+        // TODO: check for empty ['page'] param
+        // $currentPage = (int) $this->sanitizeGetParams($_GET['page']) ?? 1;
+        // var_dump($this->current_page);
+        if(is_null($this->current_page)){
+            $this->current_page = (int) $this->sanitizeGetParams();
+            var_dump($this->current_page);
+            // die;    
+            if($this->current_page >= $this->getTotalPages()){
+                $this->current_page = (int) $this->getTotalPages();
+            }elseif ($this->current_page <= 0) {
+                $this->current_page =  1;
+            }
+            // else{
+            //     return $this->current_page;
+            // }
+        }
+
+        return $this->current_page;
+        // return $currentPage >= $total_pages ? $total_pages : ($_GET['page'] < 0 ? 1 : $_GET['page']); // first|last|$_GET[page] -> page        
+    }
+
+    /**
+     * TODO: refactor
+     */
+    public function currentPage()
+    {
+        return $this->current_page;
+    }
+
+    /**
+     * Get current images offset, based on the get page parameter
+     * @return float|int
+     */
+    public function getCurrentPageImageOffset() {
+        $offset = (($this->getCurrentPage() * $this->images_per_page) - $this->images_per_page) + 1; // +1 to begin counting from the 1st item       
+
+        if($this->getCurrentPage() <= 0){
+            // $offset = ((1 * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item
+            $offset = 1;
+        }
+
+        return $offset;
+    }
+
+    /**
+     * Get previous page
+     */
+    public function getPreviousPage()
+    {
+        if($this->getCurrentPage() == 1){
+            return '#';
+        }else if($this->getCurrentPage() >= $this->getTotalPages()){
+            return $this->getTotalPages() - 1;
+        }
+        return $previous_page = $this->getCurrentPage() - 1;
+    }
+
+    /**
+     * Get next page
+     */
+    public function getNextPage($last_page = false)
+    {
+        $uri = "index.php?page=";
+        if($this->getCurrentPage() == $this->getTotalPages()){
+            return '#';        
+        }elseif ($last_page == true){
+            return $next_page = "{$uri}{$this->getTotalPages()}";
+        }
+        else {
+            $next_page = $this->getCurrentPage() + 1;
+            return "{$uri}{$next_page}";
+        }
+        // return $next_page = $this->getCurrentPage() <= 0 ? 2 : $this->getCurrentPage() + 1;        
+    }
+
+    /**
+     * Sanitize input
+     * @param mixed $input
+     * @return mixed
+     */
+    public function sanitizeGetParams()
+    {
+        // var_dump($input);        
+        $availableGetOptions = array('page');
+        $sanitizedParams = [];
+
+        if(!empty($_GET)){            
+            // var_dump($_GET);
+            foreach ($availableGetOptions as $option) {
+                if(!array_key_exists($option, $_GET)){
+                    header('Location: index.php'); // return ('Get param not existing!'); // TODO: header with html-msg ? 
+                    die;
+                }
+                $sanitizedParams[$option] = htmlspecialchars($_GET[$option], ENT_QUOTES);
+            }
+        }
+        // sanitize
+        return $sanitizedParams;
+    }
+
     /////////////////////////////
-    // public function paginate()
-    // {
-    //     echo 'test paginate';
-    // }
+    public function paginate()
+    {
+        echo 'test paginate';
+    }
 }
--- templates/content.php
+++ templates/content.php
@@ -1,90 +1,91 @@
 <?php
-// $paginator = new Paginator();
-// $paginator->paginate();
+$paginator = new Paginator();
+echo $paginator->currentPage();
+// die;
 
-    $total_images = 50; // total item incoming API, Database, CONST int ...
+//     $total_images = 50; // total item incoming API, Database, CONST int ...
 
-    $images_per_page = 12; // ?User Input -- our needs for the grid
-    // $images_per_row = 4; // ?? CONST for easy and more gridable rows our needs for the grid
+//     $images_per_page = 12; // ?User Input -- our needs for the grid
+//     // $images_per_row = 4; // ?? CONST for easy and more gridable rows our needs for the grid
 
-    $total_pages = ceil($total_images/$images_per_page);
-    // $total_rows = ceil($images_per_page / $images_per_row);
-    $total_rows = ceil($images_per_page / 4); // total rows is default=hardcoded 
+//     $total_pages = ceil($total_images/$images_per_page);
+//     // $total_rows = ceil($images_per_page / $images_per_row);
+//     $total_rows = ceil($images_per_page / 4); // total rows is default=hardcoded 
     
-    $current_page = $_GET['page'] >= $total_pages ? $total_pages : ($_GET['page'] < 0 ? 1 : $_GET['page']); // first|last|$_GET[page] -> page
-    $previous_page = $current_page - 1;
-    $next_page = $current_page <= 0 ? 2 : $current_page + 1;
+//     $current_page = $_GET['page'] >= $total_pages ? $total_pages : ($_GET['page'] < 0 ? 1 : $_GET['page']); // first|last|$_GET[page] -> page
+//     $previous_page = $current_page - 1;
+//     $next_page = $current_page <= 0 ? 2 : $current_page + 1;
 
-    $counter_images_current_page = get_current_page_image_offset($current_page, $images_per_page);
+//     $counter_images_current_page = get_current_page_image_offset($current_page, $images_per_page);
     
-    // Get the current page offset of items
-    function get_current_page_image_offset($current_page, $images_per_page) {
-        $offset = (($current_page * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item       
+//     // Get the current page offset of items
+//     function get_current_page_image_offset($current_page, $images_per_page) {
+//         $offset = (($current_page * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item       
 
-        if($current_page <= 0){
-            // $offset = ((1 * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item
-            $offset = 1;
-        }
+//         if($current_page <= 0){
+//             // $offset = ((1 * $images_per_page) - $images_per_page) + 1; // +1 to begin counting from the 1st item
+//             $offset = 1;
+//         }
 
-        return $offset;
-    };
+//         return $offset;
+//     };
 
-function nextPage($current_page, $total_pages, $last_page = false){
-    $uri = "index.php?page=";
-    if($current_page == $total_pages){
-        return '#';        
-    }elseif ($last_page == true){
-        return $next_page = "{$uri}{$total_pages}";
-    }
-    else {
-        $next_page = $current_page + 1;
-        return "{$uri}{$next_page}";
-    }
-}
+// function nextPage($current_page, $total_pages, $last_page = false){
+//     $uri = "index.php?page=";
+//     if($current_page == $total_pages){
+//         return '#';        
+//     }elseif ($last_page == true){
+//         return $next_page = "{$uri}{$total_pages}";
+//     }
+//     else {
+//         $next_page = $current_page + 1;
+//         return "{$uri}{$next_page}";
+//     }
+// }
 
-function previousPage($current_page, $total_pages){
-    if($current_page == 1){
-        return '#';
-    }else if($current_page >= $total_pages){
-        return $total_pages - 1;
-    }
-}
+// function previousPage($current_page, $total_pages){
+//     if($current_page == 1){
+//         return '#';
+//     }else if($current_page >= $total_pages){
+//         return $total_pages - 1;
+//     }
+// }
 
-// TODO: to be replaced by Pagination class methods
-// Get previous page class
-$previous_page_class = function() use ($current_page) {
-    return $current_page <= 1 ? 'disabled' : '';
-};
+// // TODO: to be replaced by Pagination class methods
+// // Get previous page class
+// $previous_page_class = function() use ($current_page) {
+//     return $current_page <= 1 ? 'disabled' : '';
+// };
 
-// Get next page class
-$next_page_class = function () use ($current_page, $total_pages) {
-    return $current_page >= $total_pages ? "disabled" : "";    
-};
+// // Get next page class
+// $next_page_class = function () use ($current_page, $total_pages) {
+//     return $current_page >= $total_pages ? "disabled" : "";    
+// };
 
-// Get last page class
-$last_page_class = function () use ($current_page, $total_pages) {
-    return $current_page >= $total_pages ? "link-danger" : "";
-};
+// // Get last page class
+// $last_page_class = function () use ($current_page, $total_pages) {
+//     return $current_page >= $total_pages ? "link-danger" : "";
+// };
 
 // var_dump($previous_page_class($current_page), $current_page, $total_pages);
 // echo $counter_images_current_page;
     echo '<div class="container text-center">';
     
-    for ($i = 0; $i < $total_rows; $i++) {                
+    for ($i = 0; $i < $paginator->getTotalRows(); $i++) {                
         echo '<div class="row">'; // start row                
         for($k = 0; $k < 4; $k++){ // items 
-            if ($counter_images_current_page <= $total_images) {
+            if ($paginator->getCurrentPageImageOffset() <= $paginator::$total_images) {
                 echo '<div class="col">
                     <div class="card">
                         <img src="'.$base->getAssetsPath().'/bottle.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title -- '.$counter_images_current_page.'</h5>
+                            <h5 class="card-title">Card title -- '.$paginator->getCurrentPageImageOffset().'</h5>
                             <p class="card-text">Card content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>';
-                $counter_images_current_page++;
+                $paginator->getCurrentPageImageOffset() + 1;
             }else{
                 echo '<div class="col"></div>'; // empty img col
             }
@@ -99,29 +100,29 @@ $last_page_class = function () use ($current_page, $total_pages) {
          <ul class="pagination pagination-lg justify-content-center">';
     // <!-- Previous Page -->
         echo'<li class="page-item">
-            <a class="page-link '.$previous_page_class($current_page) .'" href="index.php?page='.previousPage($current_page, $total_pages).'">Previous</a>
+            <a class="page-link '.$paginator->getPreviousPageClass() .'" href="index.php?page='. $paginator->getPreviousPage().'">Previous</a>
         </li>';
     // <!-- Iterrator pages -->
         echo'<li class="page-item" data-activepage="1"><a class="page-link" href="index.php?page=1">1</a></li>
             <li class="page-item" data-activepage="2"><a class="page-link" href="index.php?page=2">2</a></li>';
         
-        if($current_page >= 3){
+        if($paginator->getCurrentPage() >= 3){
             echo'<li class="page-item"><a class="page-link" href="#">...</a></li>
-            <li class="page-item" data-activepage="'. $current_page .'"><a class="page-link" href="index.php?page='. $current_page .'">'.$current_page.'</a></li>';
-         }
+            <li class="page-item" data-activepage="'. $paginator->getCurrentPage() .'"><a class="page-link" href="index.php?page='. $paginator->getCurrentPage() .'">'.$paginator->getCurrentPage().'</a></li>';
+            }
         
     // <!-- Next Page -->
-    echo '<li class="page-item">                    
-        <a class="page-link '. $next_page_class($current_page, $total_pages) .'"  
-        href="'. nextPage($current_page, $total_pages) .'">Next</a>
-    </li>';
+        echo '<li class="page-item">                    
+            <a class="page-link '. $paginator->getNextPageClass() .'"  
+            href="'. $paginator->getNextPage() .'">Next</a>
+        </li>';
 
     // <!-- Last Page -->
-    echo '<li class="page-item">
-        <a class="page-link '. $last_page_class($current_page, $total_pages) .'" href="'. nextPage($current_page, $total_pages, true) .'">Last Page</a>
-    </li>';
+        echo '<li class="page-item">
+            <a class="page-link '. $paginator->getLastPageClass() .'" href="'. $paginator->getNextPage(true) .'">Last Page</a>
+        </li>';
 
-    echo'</ul>
+        echo'</ul>
         </nav>
     </div>';
 ?>
